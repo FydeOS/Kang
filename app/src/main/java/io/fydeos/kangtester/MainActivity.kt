4d9530e657b6a2c86e3c5cb26672347621ee531b
t123yh <t123yh@outlook.com>
2023-03-07 18:15:39 +0800

    Add analytics

--- app/src/main/java/io/fydeos/kangtester/MainActivity.kt
+++ app/src/main/java/io/fydeos/kangtester/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.analytics.FirebaseAnalytics
 import io.fydeos.kangtester.databinding.ActivityMainBinding
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -18,6 +22,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private val navListener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+        val bundle = Bundle()
+        val currentFragmentClassName = (controller.currentDestination as FragmentNavigator.Destination).className
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_NAME, destination.label.toString())
+        bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, currentFragmentClassName)
+        FirebaseAnalytics.getInstance(this).logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,11 +39,19 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController.addOnDestinationChangedListener(navListener)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
         val dateFormat: DateFormat = SimpleDateFormat("yy-MM-dd HH:mm", Locale.US)
 
         binding.content.tvBuild.text = getString(R.string.app_version).format(BuildConfig.VERSION_NAME, dateFormat.format(Date(BuildConfig.BUILD_TIME.toLong())))
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController.removeOnDestinationChangedListener(navListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
